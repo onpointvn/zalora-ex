16ea31c0c0f41435dbb62de3958efbd2c5348011
Khanh Nguyen <nguyenkhanh@onpoint.vn>
2022-12-16 16:48:01 +0700

    Add getting products API

---
+++ lib/api/product.ex
@@ -0,0 +1,24 @@
+defmodule Zalora.Product do
+  alias Zalora.Client
+
+  @doc """
+  Get list of product for a product set
+
+  https://sellercenter-api.zalora.com.ph/docs/#/Product/get_v2_product_set__productSetId__products
+  """
+  @spec get_product_set_products(product_set_id :: integer(), opts :: Keyword.t()) ::
+          {:ok, list(map())} | {:error, any()}
+  def get_product_set_products(product_set_id, opts \\ []) do
+    with {:ok, client} <- Client.new(opts),
+         {:ok, items} when is_list(items) <-
+           Client.get(client, "/v2/product-set/#{product_set_id}/products") do
+      {:ok, items}
+    else
+      {:ok, data} ->
+        {:error, data}
+
+      error ->
+        error
+    end
+  end
+end
