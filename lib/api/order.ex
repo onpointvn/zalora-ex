9db1a0686c55a3c5fd8700654830850f1d6f658f
Khanh Nguyen <nguyenkhanh@onpoint.vn>
2023-01-05 15:41:26 +0700

    Refactor code

--- lib/api/order.ex
+++ lib/api/order.ex
@@ -335,6 +335,60 @@ defmodule Zalora.Order do
   end
 
   @doc """
+  Get order items by given order items ids or order ids
+
+  Reference
+
+  https://sellercenter-api.zalora.com.ph/docs/#/Orders/GET_v2-order-items-get
+  """
+  @get_order_items_schema %{
+    order_item_ids: [type: {:array, :integer}, length: [min: 1]],
+    order_ids: [type: {:array, :integer}, length: [min: 1]]
+  }
+  def get_order_items(params, opts \\ []) do
+    with {:ok, query} <- Contrak.validate(params, @get_order_items_schema),
+         {:ok, client} <- Client.new(opts) do
+      query =
+        query
+        |> MapHelper.clean_nil()
+        |> MapHelper.to_request_data()
+
+      client
+      |> Client.get("/v2/order-items", query: query)
+      |> case do
+        {:ok, items} = result when is_list(items) ->
+          result
+
+        {:ok, data} ->
+          {:error, data}
+
+        error ->
+          error
+      end
+    end
+  end
+
+  @doc """
+  Get shipment provider
+
+  Reference
+
+  https://sellercenter-api-staging.zalora.com.ph/docs/#/Orders/GET_v2-orders-shipment-providers
+  """
+  def get_shipment_providers(opts \\ []) do
+    with {:ok, client} <- Client.new(opts),
+         {:ok, items} when is_list(items) <- Client.get(client, "/v2/orders-shipment-providers") do
+      {:ok, items}
+    else
+      {:ok, data} ->
+        {:error, data}
+
+      error ->
+        error
+    end
+  end
+
+  @doc """
   Set orders items statuses to "packed by marketplace"
 
   Reference
