fb2897c09041800500f226de6c4bf19da20dffdb
Phuoc Huynh <phuoc.huynh@onpoint.vn>
2023-03-13 10:26:36 +0700

    implement api get payment methods

---
+++ lib/api/payment_method.ex
@@ -0,0 +1,23 @@
+defmodule Zalora.PaymentMethod do
+  alias Zalora.Client
+
+  @doc """
+
+  https://sellercenter-api.zalora.com.ph/docs/#/Payment%20Method/get_v2_payment_methods
+  """
+  @spec get_payment_methods(opts :: Keyword.t()) ::
+          {:ok, list(map())} | {:error, any()}
+  def get_payment_methods(opts \\ []) do
+    with {:ok, client} <- Client.new(opts) do
+      client
+      |> Client.get("/v2/payment-methods")
+      |> case do
+        {:ok, _} = result ->
+          result
+
+        error ->
+          error
+      end
+    end
+  end
+end
