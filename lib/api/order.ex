a2a8fe1ed61fc3c064c0d7eaeb5e99b7171b1e07
Phuoc Huynh <phuoc.huynh@onpoint.vn>
2023-09-21 17:40:35 +0700

    add more api download file

--- lib/api/order.ex
+++ lib/api/order.ex
@@ -480,6 +480,30 @@ defmodule Zalora.Order do
   end
 
   @doc """
+  Download file managers
+
+  Reference
+  https://sellercenter-api.zalora.com.ph/docs/#/Files/get_filemanager_v1_files_download__uuid_
+  """
+  @download_manager_file_schema %{
+    uuid: :string
+  }
+  def download_manager_file(params, opts \\ []) do
+    with {:ok, %{uuid: uuid}} <- Contrak.validate(params, @download_manager_file_schema),
+         {:ok, client} <- Client.new(opts),
+         result <-
+           Client.get(client, "filemanager/v1/files/download/#{uuid}") do
+      result
+    else
+      {:ok, data} ->
+        {:error, data}
+
+      error ->
+        error
+    end
+  end
+
+  @doc """
   Set to ready to ship
 
   Reference
